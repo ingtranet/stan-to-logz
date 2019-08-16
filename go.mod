4a7693984c224c0df760545fd0061388e25059a9
cookieshake <cookieshake.dev@gmail.com>
2019-08-17 00:43:45 +0900

    add some codes

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/ingtranet/stan-to-logz
+
+go 1.12
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/beeker1121/goque v2.0.1+incompatible // indirect
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/logzio/logzio-go v0.0.0-20190421083739-334c774b7aeb
+	github.com/nats-io/stan.go v0.5.0
+	github.com/shirou/gopsutil v2.18.12+incompatible // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	golang.org/x/sys v0.0.0-20190813064441-fde4db37ae7a // indirect
+)
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"github.com/logzio/logzio-go"
+	"github.com/nats-io/stan.go"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	logzioToken := os.Getenv("LOGZIO_TOKEN")
+	clusterID := os.Getenv("CLUSTER_ID")
+	natsURL := os.Getenv("NATS_URL")
+	subject := os.Getenv("SUBJECT")
+	queueGroup := os.Getenv("QUEUE_GROUP")
+	durableName := queueGroup
+
+	l, err := logzio.New(
+		logzioToken,
+		logzio.SetDebug(os.Stderr),
+		logzio.SetUrl("https://listener.logz.io:8071"),
+		logzio.SetDrainDuration(time.Second*10),
+		logzio.SetTempDirectory("/tmp/logzio"),
+		logzio.SetDrainDiskThreshold(99),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var clientID string
+	hostname, err := os.Hostname()
+	if err != nil {
+		clientID = hostname
+	} else {
+		clientID = "client"
+	}
+
+
+	qcb := func (m *stan.Msg) {
+		if err := l.Send(m.Data); err != nil {
+			println(err.Error())
+		}
+	}
+
+	sc, err := stan.Connect(clusterID, clientID, stan.NatsURL(natsURL))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	sub, err := sc.QueueSubscribe(subject, queueGroup, qcb, stan.DurableName(durableName))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigs
+
+	sub.Close()
+	sc.Close()
+	l.Stop()
+}
